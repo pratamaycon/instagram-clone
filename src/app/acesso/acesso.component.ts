2408157dd9a4adef9ed1958c95cac385b192d625
Maycon <mayconprata90@gmail.com>
2020-04-05 18:07:38 -0300

    criado componentes

---
+++ src/app/acesso/acesso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acesso',
+  templateUrl: './acesso.component.html',
+  styleUrls: ['./acesso.component.sass']
+})
+export class AcessoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
